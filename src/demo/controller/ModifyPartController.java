fdcb2d8406d87ca2334140f9a42075b4526bd7f0
Justin R. Zweifel <47803568+Jzweifel15@users.noreply.github.com>
2023-04-20 19:24:28 -0400

    Added Modify Part View Controls
    Added view controls to the controller

--- src/demo/controller/ModifyPartController.java
+++ src/demo/controller/ModifyPartController.java
@@ -1,9 +1,26 @@
 package demo.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 public class ModifyPartController {
 
+
+    public RadioButton inHouseRadioButton;
+    public RadioButton outsourcedRadioButton;
+    public TextField idTextField;
+    public TextField nameTextField;
+    public TextField inventoryTextField;
+    public TextField priceCostTextField;
+    public TextField maxTextField;
+    public TextField machineIdOrCompanyNameTextField;
+    public Label minLabel;
+    public TextField minTextField;
+    public Button savePartButton;
+    public Button CancelPartButton;
 
     public void onCancel(ActionEvent actionEvent) {
     }
