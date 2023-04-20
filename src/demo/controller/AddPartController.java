e56ad21c24dd6934be3c28e75900fdabbbd689f1
Justin R. Zweifel <jzweifel15@yahoo.com>
2023-04-20 16:10:46 -0400

    Save Button ActionEvent
    Created event handler for the save button in the Add Part Form

--- src/demo/controller/AddPartController.java
+++ src/demo/controller/AddPartController.java
@@ -1,5 +1,6 @@
 package demo.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
@@ -12,7 +13,7 @@ import java.util.ResourceBundle;
 public class AddPartController implements Initializable {
 
     public AnchorPane addPartFormPane;
-    public RadioButton inHouseRadioButton;
+    public RadioButton inHouseRadioButton;      // isSelected() is the method used for checking if a radio button is selected
     public RadioButton outsourcedRadioButton;
     public TextField idTextField;
     public TextField nameTextField;
@@ -30,4 +31,10 @@ public class AddPartController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {}
 
+    /**
+     * The action event that is triggered when the user tries to save a new Part
+     * @param actionEvent an event Object
+     */
+    public void savePartButton(ActionEvent actionEvent) {
+    }
 }
