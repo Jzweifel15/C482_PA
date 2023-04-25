6f5e793ba6aeb7ac68df17324a7bac0844ca738c
Justin R. Zweifel <jzweifel15@yahoo.com>
2023-04-25 16:37:07 -0400

    Added FXML Controls
    Added the associated FXML IDs for the add-product-view form controls

--- src/demo/controller/AddProductController.java
+++ src/demo/controller/AddProductController.java
@@ -1,4 +1,33 @@
 package demo.controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class AddProductController {
+    public TextField nameTextField;
+    public TextField idTextField;
+    public TextField inventoryTextField;
+    public TextField PriceCostTextField;
+    public TextField maxTextField;
+    public TextField searchPartTextField;
+    public TextField minTextField;
+
+    public TableView partTableView1;
+    public TableColumn partIdTable1Column;
+    public TableColumn partNameTable1Column;
+    public TableColumn inventoryLevelTable1Column;
+    public TableColumn pricePerUnitTable1Column;
+
+    public TableView partTableView2;
+    public TableColumn partIdTable2Column;
+    public TableColumn partNameTable2Column;
+    public TableColumn inventoryLevelTable2Column;
+    public TableColumn pricePerUnitTable2Column;
+
+    public Button addAssociatedPartButton;
+    public Button removeAssociatedPartButton;
+    public Button saveProductButton;
+    public Button cancelProductButton;
 }
