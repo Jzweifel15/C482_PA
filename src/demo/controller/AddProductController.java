45c7564676005c284a2ac20da6ca9ae2b0b1378e
Justin R. Zweifel <jzweifel15@yahoo.com>
2023-04-30 21:47:43 -0400

    Update AddProductController.java
    Added the functionality to the Add Associated Part button which will take a part from the top TableView (the all parts table) and move it to the bottom TableView (the associated parts table)

--- src/demo/controller/AddProductController.java
+++ src/demo/controller/AddProductController.java
@@ -2,6 +2,9 @@ package demo.controller;
 
 import demo.model.Inventory;
 import demo.model.Part;
+import demo.model.Product;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -17,11 +20,15 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collections;
 import java.util.ResourceBundle;
 
 public class AddProductController implements Initializable {
 
     private Inventory inventory;
+
+    private ObservableList<Part> allParts;
+    private ObservableList<Part> associatedParts;
 
     public TextField nameTextField;
     public TextField idTextField;
@@ -50,21 +57,40 @@ public class AddProductController implements Initializable {
 
     public AddProductController(Inventory inventory) {
         this.inventory = inventory;
+        this.allParts = FXCollections.observableArrayList(inventory.getAllParts());
+        this.associatedParts = FXCollections.observableArrayList();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // Add all Parts in inventory to the top TableView
-        partTableView1.setItems(this.inventory.getAllParts());
+        partTableView1.setItems(this.allParts);
         partIdTable1Column.setCellValueFactory(new PropertyValueFactory<>("id"));
         partNameTable1Column.setCellValueFactory(new PropertyValueFactory<>("name"));
         inventoryLevelTable1Column.setCellValueFactory(new PropertyValueFactory<>("stock"));
         pricePerUnitTable1Column.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        // Prepare the bottom TableView to hold the Associated Parts for if/when an associated part is added
+        partTableView2.setItems(this.associatedParts);
+        partIdTable2Column.setCellValueFactory(new PropertyValueFactory<>("id"));
+        partNameTable2Column.setCellValueFactory(new PropertyValueFactory<>("name"));
+        inventoryLevelTable2Column.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        pricePerUnitTable2Column.setCellValueFactory(new PropertyValueFactory<>("price"));
     }
 
     // FIXME: Add the rest of the logic to add an associated part for a new Product
     public void addAssociatedPart(ActionEvent actionEvent) throws IOException {
         Part partSelected = (Part) partTableView1.getSelectionModel().getSelectedItem();
+
+        if (partSelected == null) {
+            return;
+        }
+
+        // Remove selectedPart from top TableView (All Parts)
+        allParts.remove(partSelected);
+
+        // Move selectedPart to bottom TableView (Associated Parts)
+        associatedParts.add(partSelected);
     }
 
     /**
