bf9236dcb38ad3c6d368d7d15d77cea9210b90d3
Justin R. Zweifel <jzweifel15@yahoo.com>
2023-05-03 04:14:30 -0400

    Generated Javadocs
    Generated javadoc comments to html using built-in tools in Intellij

---
+++ javadocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"Demo","l":"demo"},{"m":"Demo","l":"demo.controller"},{"m":"Demo","l":"demo.model"}];updateSearchResults();
